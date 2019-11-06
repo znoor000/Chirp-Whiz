f058e121587ef5709a7e20076bb622b083fd7b7a
Luis Medina <lmedina004@citymail.cuny.edu>
2019-11-05 20:18:48 -0500

    Added submiting logic to selection.js

--- selection.js
+++ selection.js
@@ -1,10 +1,28 @@
 const choices = document.querySelector('div.options');
+const ansButton = document.querySelector('button.answer');
+let hasAnswered = false;
 
 choices.addEventListener('click', event => {
-  for (let i = 0; i < choices.children.length; ++i){
-    choices.children[i].className = '';
+  // If user has not submited answer yet
+  if(!hasAnswered){
+    // Clear out previous selection
+    for (let i = 0; i < choices.children.length; ++i){
+      choices.children[i].className = '';
+    }
+    // If user clicked on an img tag, give that tag a class
+    if(event.target.tagName === "IMG"){
+      event.target.className = "selected";
+    }
   }
-  if(event.target.tagName === "IMG"){
-    event.target.className = "selected";
+});
+
+ansButton.addEventListener('click', () => {
+  for (let i = 0; i < choices.children.length; ++i){
+    if(choices.children[i].className == 'selected'){
+      hasAnswered = true;
+    }
+  }
+  if (!hasAnswered) {
+    alert('Please make a selection!');
   }
 });
