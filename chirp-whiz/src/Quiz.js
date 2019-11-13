fe9e22523ec76c628be4139dfbc98b1a24899dfd
Luis Medina <lmedina004@citymail.cuny.edu>
2019-11-13 05:52:08 -0500

    Added flow logic to the quiz

--- chirp-whiz/src/Quiz.js
+++ chirp-whiz/src/Quiz.js
@@ -4,11 +4,20 @@ import {birdList, habitats} from './birdInfo.js';
 class Quiz extends Component {
   choices;
   userChoice;
-  hasAnswered = false;
+  
+  constructor(){
+    super()
+
+    this.state = {
+      quizFlow: [0, 1, 0, 1, 1], // 0 is info page, 1 are quiz pages
+      quizPointer: 0,
+      hasAnswered: true
+    }
+  }
 
   handleChoices = (event) => {
     // If user has not submited answer yet
-    if(!this.hasAnswered){
+    if(!this.state.hasAnswered){
       // Clear out previous selection
       for (let i = 0; i < this.choices.children.length; ++i){
         this.choices.children[i].className = '';
@@ -26,15 +35,29 @@ class Quiz extends Component {
     // Check if user has made a selection
     for (let i = 0; i < this.choices.children.length; ++i){
       if(this.choices.children[i].className == 'selected'){
-        this.hasAnswered = true;
+        this.state.hasAnswered = true;
       }
     }
     // If user did not answer, alert them
-    if (!this.hasAnswered) {
+    if (!this.state.hasAnswered) {
       alert('Please make a selection!');
-    } else {
+    } else { // check answer is correct or wrong
       if(this.userChoice.id == "1"){this.userChoice.className = "correct"}
       else{this.userChoice.className = "wrong"}
+    }
+  }
+
+  handleNext = (event) => {
+    if(this.state.quizFlow[this.state.quizPointer] === 0){
+      this.setState((prevState) => ({
+        quizPointer: prevState.quizPointer + 1,
+        hasAnswered: false
+      }));
+    } else if (this.state.hasAnswered) {
+      this.setState((prevState) => ({
+        quizPointer: prevState.quizPointer + 1,
+        hasAnswered: false
+      }));
     }
   }
 
@@ -42,22 +65,35 @@ class Quiz extends Component {
     this.choices = document.querySelector('div.options');
   }
 
+  componentDidUpdate(){
+    this.choices = document.querySelector('div.options');
+  }
+
   render() {
-    return(
-      <div>
-        <h2>Select the bird that has this call:{this.userChoice}</h2>
-        <audio controls>
-          <source src="BirdAudios/pigeons.wav" type="audio/wav" />
-        </audio>
-        <div className="options" onClick={this.handleChoices}>
-          <img id="1" src="Birds/RockPigeon.jpg" width="200px" />
-          <img id="2" src="Birds/MourningDove.jpg" width="200px" />
-          <img id="3" src="Birds/HouseSparrow.jpg" width="200px" />
-          <img id="4" src="Birds/EuropeanStarling.jpg" width="200px" />      
+    if(this.state.quizFlow[this.state.quizPointer] === 0){
+      return(
+        <div>
+          <button className="next" onClick={this.handleNext}>Next</button>
         </div>
-        <button className="answer" onClick={this.handleAnsButton}>Submit</button>
-      </div>
-    );
+      );
+    }else{
+      return(
+        <div>
+          <h2>Select the bird that has this call:</h2>
+          <audio controls>
+            <source src="BirdAudios/pigeons.wav" type="audio/wav" />
+          </audio>
+          <div className="options" onClick={this.handleChoices}>
+            <img id="1" src="Birds/RockPigeon.jpg" width="200px" />
+            <img id="2" src="Birds/MourningDove.jpg" width="200px" />
+            <img id="3" src="Birds/HouseSparrow.jpg" width="200px" />
+            <img id="4" src="Birds/EuropeanStarling.jpg" width="200px" />      
+          </div>
+          <button className="answer" onClick={this.handleAnsButton}>Submit</button>
+          <button className="next" onClick={this.handleNext}>Next</button>
+        </div>
+      );
+    }
   }
 }
 
