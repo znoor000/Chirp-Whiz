ec679cabee3b2ab7fc95ed3272fcbbf1a5fdc6ad
Liam Boylan <liamboylan7@yahoo.com>
2019-11-17 17:16:31 -0500

    add styling

--- chirp-whiz/src/Home.js
+++ chirp-whiz/src/Home.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './Home.css';
 
 class Home extends Component {
     render() {
       return(
-        <div>
+        <div className="homepage">
           <h1>Chirp Whiz</h1>
           <h1>Home</h1>
         </div>
