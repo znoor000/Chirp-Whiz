1f9e83367b93db50b47bfd6c7d18b2df81980b13
Liam Boylan <liamboylan7@yahoo.com>
2019-11-17 23:55:50 -0500

    change audio from button to controls

--- chirp-whiz/src/AudioButton.js
+++ chirp-whiz/src/AudioButton.js
@@ -1,20 +1,13 @@
 import React from 'react';
 
 function AudioButton(props) {
-    function playAudio() { 
-      let x = document.getElementById("birdQAudio");
-      x.play();
-    }
-  
       return (
         <div>
-          <audio id="birdQAudio">
+          <audio controls id="birdQAudio">
             <source src={props.sound} type="audio/mpeg"></source>
           </audio>
-  
-          <button onClick={playAudio}>Bird Call/Sound</button>
         </div>
       );
-  }
+}
 
 export default AudioButton;
