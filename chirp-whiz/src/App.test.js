cc38da494c9fe5956acf82faac897edf77a7bd46
Liam Boylan <liamboylan7@yahoo.com>
2019-11-18 21:58:58 -0500

    integration tests

--- chirp-whiz/src/App.test.js
+++ chirp-whiz/src/App.test.js
@@ -2,8 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent } from '@testing-library/react'
+import { Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('test full app', () => {
+  const history = createMemoryHistory()
+  const { container, getByText } = render(
+    <Router history={history}>
+      <App />
+    </Router>
+  )
+  {/*
+  expect(container.innerHTML).toMatch('Chirp Whiz')
+
+  fireEvent.click(getByText('Quiz'))
+
+  expect(container.innerHTML).toMatch('Quiz')
+  */}
+})
