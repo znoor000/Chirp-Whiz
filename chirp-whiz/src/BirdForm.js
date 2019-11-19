e3f8898873e3621b89e5b2f684c7f70408b3d750
Liam Boylan <liamboylan7@yahoo.com>
2019-11-19 16:22:27 -0500

    react-bootstrap

--- chirp-whiz/src/BirdForm.js
+++ chirp-whiz/src/BirdForm.js
@@ -3,6 +3,9 @@ import API, { graphqlOperation } from '@aws-amplify/api'
 import PubSub from '@aws-amplify/pubsub';
 import { createTodo } from './graphql/mutations'
 import config from './aws-exports'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 API.configure(config);
 PubSub.configure(config);
@@ -41,29 +44,33 @@ function BirdForm() {
     return(
         <div>
             <h1>Bird Form</h1>
+        <Card border="primary">
+        <Card.Body>
             <form>
                 <label>
-                    Name of bird:
+                    Name of bird:<br/>
                     <input name="birdName" type="text" onChange={fillingForm}/>
                 </label><br />
                 <label>
-                    Image of bird:
+                    Image of bird:<br/>
                     <input name="birdImage" type="text" onChange={fillingForm}/>
                 </label><br />
                 <label>
-                    Sound clip of bird:
+                    Sound clip of bird:<br/>
                     <input name="birdSound" type="text" onChange={fillingForm}/>
                 </label><br />
                 <label>
-                    Habitat image of bird:
+                    Habitat image of bird:<br/>
                     <input name="habitatImage" type="text" onChange={fillingForm}/>
                 </label><br />
                 <label>
-                    Habitat sound of bird:
+                    Habitat sound of bird:<br/>
                     <input name="habitatSound" type="text" onChange={fillingForm}/>
                 </label><br />
             </form>
-            <button onClick={() => createNewTodo(newBird)}>Add Bird</button>
+            <Button variant="outline-light" style={{backgroundColor: "#ffa333"}} onClick={() => createNewTodo(newBird)}>Add Bird</Button>
+      </Card.Body>
+      </Card>
         </div>
     );
 }
