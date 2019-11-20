573526190281fb96f8ae4e747f6580ec1c3e79b1
Zohora Noor <znoor000@citymail.cuny.edu>
2019-11-20 11:32:49 -0500

    add test: update bird with blank info

--- chirp-whiz/src/BirdForm.test.js
+++ chirp-whiz/src/BirdForm.test.js
@@ -3,3 +3,8 @@ import '@testing-library/jest-dom/extend-expect'
 import { render } from '@testing-library/react'
 import BirdForm from './BirdForm'
 
+const BirdLib = require('./BirdForm');
+
+test('update bird with blank info', () => {});
+
+
