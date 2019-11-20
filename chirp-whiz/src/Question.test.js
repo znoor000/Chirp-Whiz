dbc5a6ba30f6655fc87f6fa98d39643218b3bc64
Luis Medina <lmedina004@citymail.cuny.edu>
2019-11-20 12:27:43 -0500

    Added the needed import statements

--- chirp-whiz/src/Question.test.js
+++ chirp-whiz/src/Question.test.js
@@ -1,5 +1,11 @@
+import React from 'react';
 import '@testing-library/jest-dom/extend-expect'
-
+import { render } from '@testing-library/react'
+import AudioButton from './AudioButton.js';
+import Question from './Question.js';
+import { AnswerButton } from './Question';
+import Button from 'react-bootstrap/Button'
+import Image from 'react-bootstrap/Image'
 const QuestionInfo = require('./Question.js');
 
 test('Check answer correct', () => {
