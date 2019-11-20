806a4fe5c2619a6ab92d392f6dd6b6b65319bcf1
Zohora Noor <znoor000@citymail.cuny.edu>
2019-11-20 11:38:32 -0500

    set up expectation function in the tests

--- chirp-whiz/src/BirdForm.test.js
+++ chirp-whiz/src/BirdForm.test.js
@@ -5,10 +5,18 @@ import BirdForm from './BirdForm'
 
 const BirdLib = require('./BirdForm');
 
-test('update bird with blank info', () => {});
+test('update bird with blank info', () => {
+    expect().toStrictEqual()
+});
 
-test('update bird with only name', () => {});
+test('update bird with only name', () => {
+    expect().toStrictEqual()
+});
 
-test('update bird with only image', () => {});
+test('update bird with only image', () => {
+    expect().toStrictEqual()
+});
 
-test('update bird that already has info', () => {});
+test('update bird that already has info', () => {
+    expect().toStrictEqual()
+});
