a37c04572466740b938baae592dd8d0ddd3dca61
Hangill Chong <wjdgksrlf122293@gmail.com>
2019-11-25 15:24:30 -0500

    changed imports of Quiz.js

--- chirp-whiz/src/Quiz.js
+++ chirp-whiz/src/Quiz.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import Question from './Question.js';
-import {
-  Switch,
-  Route,
-  Link,
-  useRouteMatch
-} from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import birdList from './birdList';
+import QuizQuestion from './quizComponents/QuizQuestion';
+import ResultPage from './quizComponents/ResultPage';
+import AudioButton from './quizComponents/AudioButton';
 import Button from 'react-bootstrap/Button';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import Image from 'react-bootstrap/Image';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Quiz() {
     let match = useRouteMatch();
