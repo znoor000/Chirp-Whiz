cc5f6d474179c80dce74f7afd3e5a56886b5b462
Zohora Noor <znoor000@citymail.cuny.edu>
2019-11-25 16:39:48 -0500

    Fixed birdList - especially habitats

--- chirp-whiz/src/birdList.js
+++ chirp-whiz/src/birdList.js
@@ -1,33 +1,33 @@
 var birdList = [
     {
         name: 'Evening grosbeak',
-        image: 'https://www.allaboutbirds.org/guide/assets/photo/120746601-480px.jpg',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/120746601-720px.jpg',
         sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h5140so.mp3',
-        habitat: ['Forest', 'Backyard']
+        habitat: 'Forests'
     },
     {
         name: 'American goldfinch',
-        image: 'https://www.allaboutbirds.org/guide/assets/photo/75242471-720px.jpg',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/124706471-720px.jpg',
         sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h5290so.mp3',
-        habitat: ['Field','Orchard','Backyard']
+        habitat: 'Open Woodlands'
     },
     {
         name: 'House finch',
-        image: 'https://www.allaboutbirds.org/guide/assets/photo/172370871-720px.jpg',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/63736771-720px.jpg',
         sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h5190so.mp3',
-        habitat: ['Desert','Field','Forest','Streamside']
+        habitat: 'Towns'
     },
     {
         name: 'Baltimore oriole',
-        image: 'https://www.allaboutbirds.org/guide/assets/photo/75258971-720px.jpg',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/63734201-720px.jpg',
         sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h5070so.mp3',
-        habitat: ['Forest','Backyard']
+        habitat: 'Open Woodlands'
     },
     {
         name: 'Orchard oriole',
-        image: 'https://www.allaboutbirds.org/guide/assets/photo/75712391-720px.jpg',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/67361081-720px.jpg',
         sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h5060so.mp3',
-        habitat: ['Forest','Orchard','River Edge']
+        habitat: 'Open Woodlands'
     }
 ];
 
