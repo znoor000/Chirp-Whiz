cec920a0c83b68043168e42acbf0ae87b05c589c
Hangill Chong <wjdgksrlf122293@gmail.com>
2019-11-25 15:18:17 -0500

    made QuestionInfo function

--- chirp-whiz/src/quizComponents/QuizQuestion.js
+++ chirp-whiz/src/quizComponents/QuizQuestion.js
@@ -2,3 +2,12 @@ import React from 'react';
 import Image from 'react-bootstrap/Image';
 import AudioButton from './AudioButton';
 import ProgressBar from 'react-bootstrap/ProgressBar';
+
+export function QuestionInfo(props) {
+    return(
+        <div>
+            <Image src={props.bird.image} thumbnail style={{height: '300px'}}/>
+            <AudioButton sound={props.bird.sound} />
+        </div>
+    );
+}
