7e875b9b7c57aa534e7cc7602df917d498fb22d3
Zohora Noor <znoor000@citymail.cuny.edu>
2019-11-26 15:01:26 -0500

    add birdType and scientificName fields for the birds

--- chirp-whiz/src/birdList.js
+++ chirp-whiz/src/birdList.js
@@ -3,31 +3,41 @@ var birdList = [
         name: 'Evening grosbeak',
         image: 'https://www.allaboutbirds.org/guide/assets/photo/120746601-720px.jpg',
         sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h5140so.mp3',
-        habitat: 'Forests'
+        habitat: 'Forests',
+        birdType: 'Grosbeak',
+        scientificName: 'Coccothraustes vespertinus'
     },
     {
         name: 'American goldfinch',
         image: 'https://www.allaboutbirds.org/guide/assets/photo/124706471-720px.jpg',
         sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h5290so.mp3',
-        habitat: 'Open Woodlands'
+        habitat: 'Open Woodlands',
+        birdType: '',
+        scientificName: ''        
     },
     {
         name: 'House finch',
         image: 'https://www.allaboutbirds.org/guide/assets/photo/63736771-720px.jpg',
         sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h5190so.mp3',
-        habitat: 'Towns'
+        habitat: 'Towns',
+        birdType: '',
+        scientificName: ''
     },
     {
         name: 'Baltimore oriole',
         image: 'https://www.allaboutbirds.org/guide/assets/photo/63734201-720px.jpg',
         sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h5070so.mp3',
-        habitat: 'Open Woodlands'
+        habitat: 'Open Woodlands',
+        birdType: '',
+        scientificName: ''
     },
     {
         name: 'Orchard oriole',
         image: 'https://www.allaboutbirds.org/guide/assets/photo/67361081-720px.jpg',
         sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h5060so.mp3',
-        habitat: 'Open Woodlands'
+        habitat: 'Open Woodlands',
+        birdType: '',
+        scientificName: ''
     }
 ];
 
