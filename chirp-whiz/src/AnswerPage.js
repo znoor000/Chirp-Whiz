f1ce14b89ab6d566c24ab74353681f99d0269895
Zohora Noor <znoor000@citymail.cuny.edu>
2019-11-26 14:32:30 -0500

    add skeleton of card and head of card (bird-name)

--- chirp-whiz/src/AnswerPage.js
+++ chirp-whiz/src/AnswerPage.js
@@ -11,7 +11,12 @@ function AnswerPage(props) {
             ) : (
                 <h2>Incorrect...</h2>
             )}
-
+            <Card>
+            <Card.Header as="h2">{props.bird.name}</Card.Header>
+            <Card.Body>
+                
+            </Card.Body>
+            </Card>
         </div>
     );
 }
