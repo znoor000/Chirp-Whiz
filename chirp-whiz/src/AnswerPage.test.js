df062691e53b19d1fe581ef73be85a19c3b15780
Zohora Noor <znoor000@citymail.cuny.edu>
2019-11-27 12:36:20 -0500

    make skeleton of three tests that check if answer page renders and check for correct/incorrect answer pages

--- chirp-whiz/src/AnswerPage.test.js
+++ chirp-whiz/src/AnswerPage.test.js
@@ -3,3 +3,9 @@ import AnswerPage from './quizComponents/AnswerPage';
 import birdList from './birdList';
 import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent, waitForElement } from '@testing-library/react'
+
+it('test AnswerPage renders', () => {});
+
+test("check if we're at answer page with correct answer", () => {});
+
+test("check if we're at answer page with incorrect answer", () => {});
