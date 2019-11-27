e2a8791ed79b7a6f630c81109c6098362e317d6b
Liam Boylan <liamboylan7@yahoo.com>
2019-11-27 04:57:34 -0500

    home page tests

--- chirp-whiz/src/Home.test.js
+++ chirp-whiz/src/Home.test.js
@@ -3,6 +3,8 @@ import Home from './Home';
 import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent, waitForElement } from '@testing-library/react'
 
+const goToPage = require('./Home.js');
+
 it('test Home renders', () => {
     const container = render(<Home />)
     expect(container.firstChild).toMatchSnapshot();
@@ -11,5 +13,27 @@ it('test Home renders', () => {
 test("check if we're at home", () => {
     const {getByText} = render(<Home />)
     expect(getByText("Chirp Whiz")).toBeInTheDocument();
-    fireEvent.click(getByText('Start Quiz Now'))
+    fireEvent.click(getByText('Go to quiz page'))
+});
+
+test("check if we're at home", () => {
+    const {getByText} = render(<Home />)
+    expect(getByText("Chirp Whiz")).toBeInTheDocument();
+    fireEvent.click(getByText('Check out the glossary'))
+});
+
+test('test for gotopage on false', () => {
+    const spy = jest.spyOn(window.location, 'reload')
+    spy.mockReturnValueOnce(false);
+  
+    expect(goToPage.goToPage()).toBeUndefined();
+    expect(spy).toHaveBeenCalledTimes(0);
+});
+
+test('test for gotopage on true', () => {
+    const spy = jest.spyOn(window.location, 'reload')
+    spy.mockReturnValueOnce(true);
+  
+    expect(goToPage.goToPage()).toBeUndefined();
+    expect(spy).toHaveBeenCalledTimes(0);
 });
