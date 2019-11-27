3343224e84a574abe2e3080f1ae11a87fb29851d
Zohora Noor <znoor000@citymail.cuny.edu>
2019-11-27 12:33:47 -0500

    import necessary pages/libraries to AnswerPage.test.js

---
+++ chirp-whiz/src/AnswerPage.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import AnswerPage from './quizComponents/AnswerPage';
+import birdList from './birdList';
+import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent, waitForElement } from '@testing-library/react'
