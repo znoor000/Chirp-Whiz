a764235f61f46626ae26fc61b29d1025fff41f3e
Zohora Noor <znoor000@citymail.cuny.edu>
2019-11-26 22:12:15 -0500

    limited bird list to birds of only four habitats

--- chirp-whiz/src/birdList.js
+++ chirp-whiz/src/birdList.js
@@ -16,14 +16,6 @@ var birdList = [
         scientificName: 'Carduelis tristis'        
     },
     {
-        name: 'House finch',
-        image: 'https://www.allaboutbirds.org/guide/assets/photo/63736771-720px.jpg',
-        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h5190so.mp3',
-        habitat: 'Towns',
-        birdType: 'Finch',
-        scientificName: 'Carpodacus mexicanus'
-    },
-    {
         name: 'Baltimore oriole',
         image: 'https://www.allaboutbirds.org/guide/assets/photo/63734201-720px.jpg',
         sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h5070so.mp3',
@@ -56,14 +48,6 @@ var birdList = [
         scientificName: 'Aix sponsa'
     },
     {
-        name: 'Great Blue Heron',
-        image: 'https://www.allaboutbirds.org/guide/assets/photo/60314201-720px.jpg',
-        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h1940so.mp3',
-        habitat: 'Marshes',
-        birdType: 'Heron',
-        scientificName: 'Ardea herodias'
-    },
-    {
         name: 'Double-Crested Cormorant',
         image: 'https://www.allaboutbirds.org/guide/assets/photo/66027281-720px.jpg',
         sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h1200so.mp3',
@@ -78,6 +62,182 @@ var birdList = [
         habitat: 'Lakes and Ponds',
         birdType: 'Loon',
         scientificName: 'Gavia immer'
+    },
+    {
+        name: 'Common Merganser',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/63910971-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h1290so.mp3',
+        habitat: 'Lakes and Ponds',
+        birdType: 'Duck',
+        scientificName: 'Mergus merganser'
+    },
+    {
+        name: 'Osprey',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/60320581-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h3640so.mp3',
+        habitat: 'Lakes and Ponds',
+        birdType: 'Osprey',
+        scientificName: 'Pandion haliaetus'
+    },
+    {
+        name: 'Belted Kingfisher',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/65764731-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h3900so.mp3',
+        habitat: 'Lakes and Ponds',
+        birdType: 'Kingfisher',
+        scientificName: 'Ceryle alcyon'
+    },
+    {
+        name: 'Purple Martin',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/68280401-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h6110so.mp3',
+        habitat: 'Lakes and Ponds',
+        birdType: 'Swallow',
+        scientificName: 'Progne subis'
+    },
+    {
+        name: 'Northern Bobwhite',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/65614911-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h2890so.mp3',
+        habitat: 'Grasslands',
+        birdType: 'Game Bird',
+        scientificName: 'Colinus virginianus'
+    },
+    {
+        name: 'Killdeer',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/64809651-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h2730so.mp3',
+        habitat: 'Grasslands',
+        birdType: 'Plover',
+        scientificName: 'Charadrius vociferus'
+    },
+    {
+        name: 'Eastern Kingbird',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/65684501-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h4440so.mp3',
+        habitat: 'Grasslands',
+        birdType: 'Flycatcher',
+        scientificName: 'Tyrannus tyrannus'
+    },
+    {
+        name: 'Barn Swallow',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/68123101-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h6130so.mp3',
+        habitat: 'Grasslands',
+        birdType: 'Swallow',
+        scientificName: 'Hirundo rustica'
+    },
+    {
+        name: 'Eastern Bluebird',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/63740061-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h7660so.mp3',
+        habitat: 'Grasslands',
+        birdType: 'Thrush',
+        scientificName: 'Sialia sialis'
+    },
+    {
+        name: 'Grasshopper Sparrow',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/39344721-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h5460so.mp3',
+        habitat: 'Grasslands',
+        birdType: 'Sparrow',
+        scientificName: 'Ammodramus savannarum'
+    },
+    {
+        name: 'Mourning Dove',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/60386921-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h3160so.mp3',
+        habitat: 'Open Woodlands',
+        birdType: 'Dove',
+        scientificName: 'Zenaida macroura'
+    },
+    {
+        name: 'Yellow-Billed Cuckoo',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/66038331-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h3870so.mp3',
+        habitat: 'Open Woodlands',
+        birdType: 'Cuckoo',
+        scientificName: 'Coccyzus americanus'
+    },
+    {
+        name: 'Chuck-Will`s-Widow',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/32803711-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h4160so.mp3',
+        habitat: 'Open Woodlands',
+        birdType: 'Nightjar',
+        scientificName: 'Caprimulgus carolinensis'
+    },
+    {
+        name: 'Ruby-Throated Hummingbird',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/60395561-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h4280so.mp3',
+        habitat: 'Open Woodlands',
+        birdType: 'Hummingbird',
+        scientificName: 'Archilochus colubris'
+    },
+    {
+        name: 'Ruffed Grouse',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/65615501-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h3000so.mp3',
+        habitat: 'Forests',
+        birdType: 'Game Bird',
+        scientificName: 'Bonasa umbellus'
+    },
+    {
+        name: 'Barred Owl',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/60394861-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h3680so.mp3',
+        habitat: 'Forests',
+        birdType: 'Owl',
+        scientificName: 'Strix varia'
+    },
+    {
+        name: 'Red-Bellied Woodpecker',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/64995071-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h4090so.mp3',
+        habitat: 'Forests',
+        birdType: 'Woodpecker',
+        scientificName: 'Melanerpes carolinus'
+    },
+    {
+        name: 'Eastern Wood-Pewee',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/65617351-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h4610so.mp3',
+        habitat: 'Forests',
+        birdType: 'Flycatcher',
+        scientificName: 'Contopus virens'
+    },
+    {
+        name: 'Blue Jay',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/59859171-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h4770so.mp3',
+        habitat: 'Forests',
+        birdType: 'Jay',
+        scientificName: 'Cyanocitta cristata'
+    },
+    {
+        name: 'Black-Capped Chickadee',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/60411301-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h7350so.mp3',
+        habitat: 'Forests',
+        birdType: 'Chickadee',
+        scientificName: 'Parus atricapillus'
+    },
+    {
+        name: 'Tufted Titmouse',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/63745741-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h7310so.mp3',
+        habitat: 'Forests',
+        birdType: 'Titmouse',
+        scientificName: 'Parus bicolor'
+    },
+    {
+        name: 'Red-Breasted Nuthatch',
+        image: 'https://www.allaboutbirds.org/guide/assets/photo/68040881-720px.jpg',
+        sound: 'https://www.mbr-pwrc.usgs.gov/id/htmwav/h7280so.mp3',
+        habitat: 'Forests',
+        birdType: 'Nuthatch',
+        scientificName: 'Sitta canadensis'
     }
 ];
 
