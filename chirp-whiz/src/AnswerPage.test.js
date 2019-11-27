358bb48cb65932897bd1acb56a7a06838967b919
Zohora Noor <znoor000@citymail.cuny.edu>
2019-11-27 12:39:04 -0500

    wrote body of the three answer-page tests

--- chirp-whiz/src/AnswerPage.test.js
+++ chirp-whiz/src/AnswerPage.test.js
@@ -4,8 +4,17 @@ import birdList from './birdList';
 import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent, waitForElement } from '@testing-library/react'
 
-it('test AnswerPage renders', () => {});
+it('test AnswerPage renders', () => {
+    const container = render(<AnswerPage answerType="correct" bird={birdList[0]} />)
+    expect(container.firstChild).toMatchSnapshot();
+});
 
-test("check if we're at answer page with correct answer", () => {});
+test("check if we're at answer page with correct answer", () => {
+    const {getByText} = render(<AnswerPage answerType="correct" bird={birdList[0]} />)
+    expect(getByText("Correct!")).toBeInTheDocument();
+});
 
-test("check if we're at answer page with incorrect answer", () => {});
+test("check if we're at answer page with incorrect answer", () => {
+    const {getByText} = render(<AnswerPage answerType="incorrect" bird={birdList[0]} />)
+    expect(getByText("Incorrect...")).toBeInTheDocument();
+});
