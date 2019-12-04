862db8c4659f694d726a1267c1df1a4590c46229
Hangill Chong <wjdgksrlf122293@gmail.com>
2019-12-03 23:24:42 -0500

    update Leaderboard.test.js

--- chirp-whiz/src/Leaderboard.test.js
+++ chirp-whiz/src/Leaderboard.test.js
@@ -3,6 +3,16 @@ import Leaderboard from './Leaderboard';
 import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent, waitForElement } from '@testing-library/react'
 
+const LeaderboardTest = require('./Leaderboard.js');
+
+test('randomize to return val less than or equal to 4', () => {
+    expect(LeaderboardTest.compare({score: 3}, {score: 4})).toBe(-1);
+})
+
+test('randomize to return val less than or equal to 4', () => {
+    expect(LeaderboardTest.compare({score: 4}, {score: 3})).toBe(1);
+})
+
 it('test Leaderboard renders', () => {
     const container = render(<Leaderboard
         users={{
