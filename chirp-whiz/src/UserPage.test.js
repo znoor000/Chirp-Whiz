296a2218c1c12d43305feec0e47e34d6bb0314ac
Hangill Chong <wjdgksrlf122293@gmail.com>
2019-12-03 23:28:07 -0500

    test UserPage renders

--- chirp-whiz/src/UserPage.test.js
+++ chirp-whiz/src/UserPage.test.js
@@ -2,3 +2,13 @@ import React from 'react';
 import UserPage from './UserPage';
 import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent, waitForElement } from '@testing-library/react'
+
+it('test UserPage renders', () => {
+    const container = render(<UserPage 
+        name='name'
+        email='email'
+        correct={[0,0,0]}
+        incorrect={[0,0,0]}
+      />)
+    expect(container.firstChild).toMatchSnapshot();
+});
