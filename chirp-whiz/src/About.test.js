ed5631951090245421f019519c6a5fc237c6f6f2
Zohora Noor <znoor000@citymail.cuny.edu>
2019-12-03 22:19:38 -0500

    add render test for about page

--- chirp-whiz/src/About.test.js
+++ chirp-whiz/src/About.test.js
@@ -2,3 +2,8 @@ import React from 'react';
 import About from './About';
 import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent, waitForElement } from '@testing-library/react'
+
+it('test About renders', () => {
+    const container = render(<About />)    
+    expect().toMatchSnapshot();
+});
