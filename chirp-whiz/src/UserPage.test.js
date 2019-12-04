d4ab6993099d2606a29de64b6ba83e11ecab2720
Hangill Chong <wjdgksrlf122293@gmail.com>
2019-12-03 23:27:31 -0500

    import UserPage

--- chirp-whiz/src/UserPage.test.js
+++ chirp-whiz/src/UserPage.test.js
@@ -1,4 +1,4 @@
 import React from 'react';
-import About from './About';
+import UserPage from './UserPage';
 import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent, waitForElement } from '@testing-library/react'
