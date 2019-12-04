4a02f5eb2c64d4902a45e5f46844c59f73181115
Zohora Noor <znoor000@citymail.cuny.edu>
2019-12-03 22:35:01 -0500

    add my email to the contact info too

--- chirp-whiz/src/About.js
+++ chirp-whiz/src/About.js
@@ -21,7 +21,7 @@ function About() {
             </p>
             <br/>
             <h1>Contact Us:</h1>
-            <p>Email: lboylan000@citymail.cuny.edu</p>
+            <p>Email: lboylan000@citymail.cuny.edu or znoor000@citymail.cuny.edu</p>
             <p>If you want to see a bird that we do not have on our  
                website please let us know! It would really help if 
                you can include the name of the bird and links to audio 
