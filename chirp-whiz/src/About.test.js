364054f53bc86246bfa8f9c02784a522abd4e8c8
Zohora Noor <znoor000@citymail.cuny.edu>
2019-12-03 22:17:40 -0500

    start a test file for the About page

---
+++ chirp-whiz/src/About.test.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import About from './About';
+import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent, waitForElement } from '@testing-library/react'
