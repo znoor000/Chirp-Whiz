536b4573753d98aa4244efff3d5c89ec993732c9
Hangill Chong <wjdgksrlf122293@gmail.com>
2019-12-03 22:32:22 -0500

    add imports to Leaderboard.test.js

---
+++ chirp-whiz/src/Leaderboard.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Leaderboard from './Leaderboard';
+import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent, waitForElement } from '@testing-library/react'
+
