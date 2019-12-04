658b7fddb0b0883653a71b72dcd0dead3614fe4d
Hangill Chong <wjdgksrlf122293@gmail.com>
2019-12-03 19:23:56 -0500

    make compare function

--- chirp-whiz/src/Leaderboard.js
+++ chirp-whiz/src/Leaderboard.js
@@ -1,3 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Table from 'react-bootstrap/Table';
+
+function compare(a, b) {
+    const scoreA = a.score;
+    const scoreB = b.score;
+
+    let comparison = 0;
+    if (scoreA > scoreB) {
+        comparison = 1;
+    } else if (scoreA < scoreB) {
+        comparison = -1;
+    }
+
+    return comparison;
+}
