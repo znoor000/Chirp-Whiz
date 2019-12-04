ed4abfc32398380afe813326e3d79837f3f812e9
Hangill Chong <wjdgksrlf122293@gmail.com>
2019-12-03 19:19:11 -0500

    make structure of UserPage

--- chirp-whiz/src/UserPage.js
+++ chirp-whiz/src/UserPage.js
@@ -3,3 +3,37 @@ import birdList from './birdList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Table from 'react-bootstrap/Table';
 
+function UserPage(props) {
+
+
+}
+
+return(
+    <div>
+        <h1>User Profile</h1><br />
+        <h4>Username: {props.name}</h4>
+        <h4>Email: {props.email}</h4><br /><br />
+        <h2>Quiz Statistics:</h2><br />
+        <Table striped bordered hover size="sm">
+            <thead>
+                <tr>
+                    <th>Bird</th>
+                    <th style={{width: '300px'}}>Number of times<br /><span style={{color: 'green'}}>correctly</span> identified</th>
+                    <th style={{width: '300px'}}>Number of times<br /><span style={{color: 'red'}}>incorrectly</span> identified</th>
+                    <th style={{width: '300px'}}>Accuracy</th>
+                </tr>
+            </thead>
+            <tbody>
+                {birdList.map((bird, i) =>
+                    <tr>
+                        <th>{bird.name}</th>
+                        <th>{props.correct[i]}</th>
+                        <th>{props.incorrect[i]}</th>
+                        <th>{winPercentage(i)}%</th>
+                    </tr>
+                )}
+            </tbody>
+        </Table>
+    </div>
+);
+}
