b07ab2f62c043f539ab26ff5bf57f1095e33bf37
Zohora Noor <znoor000@citymail.cuny.edu>
2019-12-03 21:43:54 -0500

    Add contact information and credit our sources

--- chirp-whiz/src/About.js
+++ chirp-whiz/src/About.js
@@ -3,10 +3,20 @@ import React from 'react';
 function About() {
     return (
         <div>
-            <h1>About:</h1>
-            <p>Insert info here...</p>
+            <h1>About this Web Application:</h1>
+            <p>_________</p>
             <h1>Contact Us:</h1>
             <p>Email: lboylan000@citymail.cuny.edu</p>
+            <p>If you want to see a bird that we do not have on our  
+               website please let us know! It would really help if 
+               you can include the name of the bird and links to audio 
+               and images of it!</p>
+            <h4>Credits:</h4>
+            <p>This website was made possible by the United States 
+                Geological Survey (USGS) and The Cornell Lab. Our 
+                bird songs have been sourced from the USGS. All 
+                other bird information and images are from The 
+                Cornell Lab's All About Birds website</p>
         </div>
     );
 }
