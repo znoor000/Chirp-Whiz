4465596b43fa97d8c79725dffe3474346fe71baa
Zohora Noor <znoor000@citymail.cuny.edu>
2019-12-03 21:54:29 -0500

    add our names to about page

--- chirp-whiz/src/About.js
+++ chirp-whiz/src/About.js
@@ -4,19 +4,30 @@ function About() {
     return (
         <div>
             <h1>About this Web Application:</h1>
-            <p>_________</p>
+            <p>Chirp Whiz started off as a simple idea for bird-lovers 
+                who are stuck in the city or don't have spare time. 
+            </p>
+            <br/>
             <h1>Contact Us:</h1>
             <p>Email: lboylan000@citymail.cuny.edu</p>
             <p>If you want to see a bird that we do not have on our  
                website please let us know! It would really help if 
                you can include the name of the bird and links to audio 
                and images of it!</p>
+            <br/>
             <h4>Credits:</h4>
             <p>This website was made possible by the United States 
                 Geological Survey (USGS) and The Cornell Lab. Our 
                 bird songs have been sourced from the USGS. All 
                 other bird information and images are from The 
-                Cornell Lab's All About Birds website</p>
+                Cornell Lab's All About Birds website.
+            </p>
+            <br/>
+            <p>Thanks for checking out this site!</p>
+            <p>Hangill Chong</p>
+            <p>Liam Boylan</p>
+            <p>Luis Medina</p>
+            <p>Zohora Noor</p>
         </div>
     );
 }
