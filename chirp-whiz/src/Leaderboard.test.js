0308616e1b0cb051f82bf16257c2dfc86ebe10e0
Hangill Chong <wjdgksrlf122293@gmail.com>
2019-12-03 23:15:09 -0500

    test Leaderboard renders

--- chirp-whiz/src/Leaderboard.test.js
+++ chirp-whiz/src/Leaderboard.test.js
@@ -3,3 +3,15 @@ import Leaderboard from './Leaderboard';
 import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent, waitForElement } from '@testing-library/react'
 
+it('test Leaderboard renders', () => {
+    const container = render(<Leaderboard
+        users={{
+            name: 'l',
+            email: 'l',
+            correct: [0,0,0],
+            incorrect: [0,0,0]
+        }}
+    />)
+    expect(container.firstChild).toMatchSnapshot();
+});
+
