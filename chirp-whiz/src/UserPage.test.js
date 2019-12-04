b86e4b85405e831dfea5ce8c0e8a87fddb950ce6
Hangill Chong <wjdgksrlf122293@gmail.com>
2019-12-03 23:31:39 -0500

    update UserPage.test.js

--- chirp-whiz/src/UserPage.test.js
+++ chirp-whiz/src/UserPage.test.js
@@ -3,6 +3,16 @@ import UserPage from './UserPage';
 import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent, waitForElement } from '@testing-library/react'
 
+const UserPageTest = require('./UserPage.js');
+
+test('randomize to return val less than or equal to 4', () => {
+    const spy = jest.spyOn(Number.prototype, 'toFixed')
+    spy.mockReturnValueOnce(3);
+  
+    expect(UserPageTest.winPercentage(1, [0,0,0], [0,0,0])).toBe(3);
+})
+
+
 it('test UserPage renders', () => {
     const container = render(<UserPage 
         name='name'
@@ -12,3 +22,4 @@ it('test UserPage renders', () => {
       />)
     expect(container.firstChild).toMatchSnapshot();
 });
+
