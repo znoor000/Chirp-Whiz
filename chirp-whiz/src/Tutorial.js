975c0c1302458951ebde35c63631b159607304ac
Luis Medina <lmedina004@citymail.cuny.edu>
2019-12-10 19:42:04 -0500

    Adding display flow logic

--- chirp-whiz/src/Tutorial.js
+++ chirp-whiz/src/Tutorial.js
@@ -1,9 +1,20 @@
 import React, { useState, useEffect } from 'react';
 
 function Tutorial() {
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+  const [quizStarted, setQuizStarted] = useState(false);
+
   return (
     <div>
-
+      {quizStarted ? (
+        <div></div>
+      ) : (
+        correctAnswers === 5 ? (
+          <div></div>
+        ) : (
+          <div></div>
+        )
+      )}
     </div>
   )
 }
