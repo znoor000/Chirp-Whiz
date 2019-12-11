763424cd6b24bba1851722ccc16ee74e6e0dac9c
Luis Medina <lmedina004@citymail.cuny.edu>
2019-12-10 22:11:15 -0500

    Added observing bird display along with BirdModal component

--- chirp-whiz/src/Tutorial.js
+++ chirp-whiz/src/Tutorial.js
@@ -25,10 +25,44 @@ export function randomize(type, oldBird) {
   }
 }
 
+function BirdModal(props) {
+  return(
+      <Modal
+          show={props.show}
+          onHide={props.onHide}
+          size="lg"
+          centered
+      >
+          <Modal.Header closeButton>
+              <Modal.Title>{birdList[props.birdNum].name}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+              <Container>
+                  <Row className="show-grid">
+                      <Col>
+              <Image src={birdList[props.birdNum].image[0]} rounded style={{height: '360px'}} />
+              </Col>
+              </Row>
+              <Row className="show-grid">
+                  <Col>
+              <AudioButton sound={birdList[props.birdNum].sound}/>
+              </Col>
+              </Row>
+              </Container>
+          </Modal.Body>
+          <Modal.Footer>
+              <Button variant="secondary" onClick={props.onHide}>Close</Button>
+          </Modal.Footer>
+      </Modal>
+  );
+}
+
 function Tutorial() {
   const [learningBirds, setLearningBirds] = useState([0, 1, 2, 3]);
   const [correctAnswers, setCorrectAnswers] = useState(0);
   const [quizStarted, setQuizStarted] = useState(false);
+  const [currentBird, setCurrentBird] = useState(0);
+  const [modalShow, setModalShow] = useState(false);
   const [correctBird, setCorrectBird] = useState(0);
 
   useEffect(() => {
@@ -45,7 +79,41 @@ function Tutorial() {
         correctAnswers === 5 ? (
           <div></div>
         ) : (
-          <div></div>
+        <div>
+          <br /><h1>Start by observing the following birds:</h1><br />
+          <Row>
+            {learningBirds.map(bird =>
+              <Col>
+              <Button
+              variant="outline-warning"
+              style={{color: 'black'}}
+              onClick={() => {
+                setCurrentBird(bird);
+                setModalShow(true);
+              }}
+              ><Image src={birdList[bird].image[0]} rounded style={{height: '200px'}} /></Button>
+              </Col>
+            )}
+          </Row>
+          <br /><br />
+          <h2>Then identify the given bird by its image and call</h2><br />
+          <Row>
+            <Col>
+              <Button
+              variant="outline-light"
+              size="lg"
+              style={{backgroundColor: "#ffa333"}}
+              onClick={() => setQuizStarted(true)}
+              >Start practice quiz</Button>
+            </Col>
+          </Row>
+
+          <BirdModal
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+            birdNum = {currentBird}
+          />
+          </div>
         )
       )}
     </div>
