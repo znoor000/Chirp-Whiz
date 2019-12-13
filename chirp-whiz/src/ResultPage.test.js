bfdd905432bfa7d7a33f16c517feaf95e15c7276
Liam Boylan <liamboylan7@yahoo.com>
2019-12-12 22:54:03 -0500

    more tests

---
+++ chirp-whiz/src/ResultPage.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ResultPage from './quizComponents/ResultPage';
+import birdList from './birdList';
+import '@testing-library/jest-dom/extend-expect'
+import { render, fireEvent, waitForElement } from '@testing-library/react'
+
+// Integration tests
+
+it('test ResultPage renders', () => {
+    const container = render(<ResultPage
+        totalQs={5}
+        correct={{}}
+        incorrect={{}}
+      />)
+    expect(container.firstChild).toMatchSnapshot();
+});
+
+test("Test for if the results page accurately portrays empty results", () => {
+    const {getByText} = render(<ResultPage
+        totalQs={5}
+        correct={{}}
+        incorrect={{}}
+      />)
+    expect(getByText("You answered 0 out of 5 correct")).toBeInTheDocument();
+});
